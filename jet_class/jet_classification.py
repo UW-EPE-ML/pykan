36b57fedf2e2e1ebcb54f8ed2da8928bb66d1f8f
yulei <zc_1994@163.com>
2024-07-01 22:09:48 +0200

    test jet classification

---
+++ jet_class/jet_classification.py
@@ -0,0 +1,83 @@
+from kan import KAN
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+import torch
+import numpy as np
+import os
+from tqdm import tqdm
+
+from util.dataloader import read_file
+
+# Load the data and concatenate the features
+x_pars, x_jets, ys = [], [], []
+
+particle_features = [
+    'part_px', 'part_py', 'part_pz', 'part_energy', 'part_deta', 'part_dphi', 'part_d0val',
+    'part_d0err', 'part_dzval', 'part_dzerr', 'part_charge'
+]
+max_num_particles = 10
+num_features_particles = len(particle_features)
+n_features = max_num_particles * num_features_particles
+output_label = [
+    'label_QCD', 'label_Hbb', 'label_Hcc', 'label_Hgg', 'label_H4q',
+    'label_Hqql', 'label_Zqq', 'label_Wqq', 'label_Tbqq', 'label_Tbl'
+]
+
+i = 0
+for jet_data in tqdm(os.listdir('data')):
+    if jet_data.endswith('.root') and '120' in jet_data:
+        x_particles, x_jet, y = read_file(
+            'data/' + jet_data,
+            max_num_particles=10,
+            particle_features=particle_features,
+            labels=output_label,
+        )
+        x_pars.append(x_particles.reshape(-1, n_features))
+        x_jets.append(x_jet)
+
+        # convert one-hot label back
+        ys.append(np.argmax(y, axis=1))
+
+        i += 1
+
+        if i == 2: break
+
+x_pars = np.concatenate(x_pars)
+x_jets = np.concatenate(x_jets)
+ys = np.concatenate(ys)
+
+# shuffle the data
+idx = np.arange(len(x_jets))
+np.random.shuffle(idx)
+x_pars = x_pars[idx]
+x_jets = x_jets[idx]
+ys = ys[idx]  # .astype(float)
+
+
+dataset = {}
+dataset['train_input'] = torch.tensor(x_pars[:1000])
+dataset['train_label'] = torch.tensor(ys[:1000])
+dataset['test_input'] = torch.tensor(x_pars[1000:2000])
+dataset['test_label'] = torch.tensor(ys[1000:2000])
+
+# create a KAN: 4-D inputs, 10-D output, and 5 hidden neurons. cubic spline (k=3), 5 grid intervals (grid=5).
+model = KAN(width=[n_features, len(output_label)], grid=3, k=3)
+model.update_grid_from_samples(dataset['train_input'])
+
+
+def train_acc():
+    return torch.mean((torch.argmax(model(dataset['train_input']), dim=1) == dataset['train_label']).float())
+
+
+def test_acc():
+    return torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).float())
+
+
+results = model.train(
+    dataset,
+    opt="LBFGS",
+    steps=200,
+    metrics=(train_acc, test_acc),
+    loss_fn=torch.nn.CrossEntropyLoss()
+)
+print(results['train_acc'][-1], results['test_acc'][-1])
